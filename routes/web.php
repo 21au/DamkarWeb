f21e30927d3a32a2a70ccbba217a55b0c308316a
donny <124661204+donnyarsyh@users.noreply.github.com>
2025-05-02 15:15:51 +0700

    test dari donny

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+//donny
 Route::get('/', function () {
     return redirect()->route('login');
 });
